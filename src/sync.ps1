cbbac0e9cbc4aa4a48ff25ec07fbd280a7145c2c
rbb <rick1stl@gmail.com>
2023-04-29 10:12:49 -0500

    dont need logs, never use them

--- src/sync.ps1
+++ src/sync.ps1
@@ -7,12 +7,13 @@ foreach ($folder in $folders){
 
     $SourceFolder = "$($srcbase)\$($folder)"
     $DestinationFolder = "$($destbase)\$($folder)"
-    $Logfile = "$($destbase)\roboCopyLogs\camera_rbb-$($folder).log"
+    # $Logfile = "$($destbase)\roboCopyLogs\camera_rbb-$($folder).log"
 
     # Write-Host " SourceFolder---: $($SourceFolder)  -----"
     # Write-Host " DestinationFolder---: $($DestinationFolder)  --"
     # Write-Host " Logfile---: $($Logfile)  --"
 
-    Robocopy $SourceFolder $DestinationFolder /LOG:$Logfile /MIR /XA:SH /XJD /R:5 /W:15 /MT:128 /V /NP
+    # Robocopy $SourceFolder $DestinationFolder /LOG:$Logfile /MIR /XA:SH /XJD /R:5 /W:15 /MT:128 /V /NP
+    Robocopy $SourceFolder $DestinationFolder /MIR /XA:SH /XJD /R:5 /W:15 /MT:128 /V /NP
 
 }
